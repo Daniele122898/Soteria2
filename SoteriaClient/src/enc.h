3998910a13a59d094a2dd6ab1749ba22824858dc
Serenity <d.palbanese@gmail.com>
2022-05-05 21:29:46 +0200

    Added AES-GCM encryption for better encryption

--- SoteriaClient/src/enc.h
+++ SoteriaClient/src/enc.h
@@ -15,5 +15,19 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
 
 void crand(unsigned char *buffer, int buffer_len);
 
+int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
+                unsigned char *aad, int aad_len,
+                unsigned char *tag,
+                unsigned char *key,
+                unsigned char *iv, int iv_len,
+                unsigned char *plaintext);
+
+int gcm_encrypt(unsigned char *plaintext, int plaintext_len,
+                unsigned char *aad, int aad_len,
+                unsigned char *key,
+                unsigned char *iv, int iv_len,
+                unsigned char *ciphertext,
+                unsigned char *tag);
+
 unsigned int sha256(unsigned char *text, int text_len, unsigned char *hashed);
 #endif //SOTERIA_ENC_H
